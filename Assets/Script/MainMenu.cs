5f896920c820db4c52270769e4ba35c5ef3755b0
Tran Van Tien <Tientvhe186986@fpt.edu.vn>
2025-10-10 10:48:41 +0700

    ending sence

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -17,4 +17,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Mainmenu");
     }
+    public void Finish()
+    {
+        SceneManager.LoadScene("FinishScene");
+    }
+
 }
